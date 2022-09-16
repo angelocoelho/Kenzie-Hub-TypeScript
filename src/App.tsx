a4e035913496f22f657a61baa6a803143ff30b0a
Ângelo Coêlho <98770132+angelocoelho@users.noreply.github.com>
2022-09-15 23:42:01 -0300

    toasty styling and configuration

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,12 @@
-import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import RoutesMain from './routes';
+import {ToastContainer } from 'react-toastify'
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+        <RoutesMain />
+        <ToastContainer autoClose={1500} position={'top-right'} hideProgressBar={false} closeOnClick={true} pauseOnHover={true} draggable={true}/>
     </div>
   );
 }
